b507cde8a36aa0d77f082402d4a073f72149d669
“shawn.hassen” <shawn.hassen@zoom.us>
2023-03-14 18:59:47 -0400

    asdf

---
+++ practice.js
@@ -0,0 +1,10 @@
+// import { log } from "./modules";
+import test from "./exp";
+// import { watermelon } from "./modules";
+// import pizza from "./modules";
+
+// pizza();
+// test();
+// console.log(test());
+test();
+console.log("hello world");
